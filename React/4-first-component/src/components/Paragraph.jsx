f80ff824627992ad15ca0fe01c71202502ba30fd
Kirandeep Kaur <kirandeepaulakh2005@gmail.com>
2026-06-15 18:06:51 +0530

    classes

---
+++ React/4-first-component/src/components/Paragraph.jsx
@@ -0,0 +1,4 @@
+const Paragraph = ( )=> {
+    return <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum cumque similique, eos iure ea minima rem veniam consectetur. Ab ad voluptate facilis, doloremque amet recusandae.</p>
+}
+export default  Paragraph;
