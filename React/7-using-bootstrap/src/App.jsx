a9086bd001e3c63f8b008dc28aebee9569713fdc
Kirandeep Kaur <kirandeepaulakh2005@gmail.com>
2026-07-07 18:01:14 +0530

    bootstrap

---
+++ React/7-using-bootstrap/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <p className="my-text">This is a piece of text</p>
+      <div className="text-primary bg-success-subtle mb-1 ms-1 me-3 mt-3 p-4 ">This is a piece of text</div>
+    <div className="text-primary bg-success-subtle m-3 p-4 fs-2 fw-normal  w-50 ">This is a piece of text</div>
+    </>
+  )
+}
+
+export default App;
