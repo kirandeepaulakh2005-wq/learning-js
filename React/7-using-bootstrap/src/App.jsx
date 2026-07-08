9e0e48dfdb25ccb18514a374ee371de085fff2c3
Kirandeep Kaur <kirandeepaulakh2005@gmail.com>
2026-07-08 19:39:59 +0530

    react

--- React/7-using-bootstrap/src/App.jsx
+++ React/7-using-bootstrap/src/App.jsx
@@ -5,8 +5,15 @@ function App() {
       <p className="my-text">This is a piece of text</p>
       <div className="text-primary bg-success-subtle mb-1 ms-1 me-3 mt-3 p-4 ">This is a piece of text</div>
     <div className="text-primary bg-success-subtle m-3 p-4 fs-2 fw-normal  w-50 ">This is a piece of text</div>
+    <div className="m-3">
+    <ul className="list-group list-group-numbered">
+      <li className="list-group-item">home</li>
+      <li className="list-group-item active">contact</li>
+      <li className="list-group-item disabled">products</li>
+    </ul>
+      </div>
     </>
   )
 }
 
-export default App;
+export default App;  
